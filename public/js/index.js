1ba1a08f88b378dda778fbd69c61aae5ef501045
iwangx <451749469@qq.com>
2015-09-20 20:49:33 +0800

    第一次上传

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wangxing on 2015/9/20.
+ */
+(function(){
+    var wrapper=document.querySelector(".swiper-wrapper");
+    window.addEventListener("resize",resize);
+    resize();
+    function resize(){
+        var winHeight=window.screen.availHeight*window.dpr;;
+        wrapper.style.height=winHeight+"px";
+    }
+    var mySwiper = new Swiper('.swiper-container', {
+        speed: 400,
+        direction:"vertical"
+    });
+})()
