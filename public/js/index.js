b1971a7c20ba96b70aafc3cade900f800b3a89fc
iwangx <451749469@qq.com>
2015-09-23 20:13:00 +0800

    0923

--- public/js/index.js
+++ public/js/index.js
@@ -3,14 +3,21 @@
  */
 (function(){
     var wrapper=document.querySelector(".swiper-wrapper");
+    var slideList=document.querySelectorAll(".swiper-slide");
     window.addEventListener("resize",resize);
     resize();
     function resize(){
-        var winHeight=window.screen.availHeight*window.dpr;;
+        var winHeight=window.screen.availHeight;
+        //var winHeight=screen.availHeight;
+        for(var i=0;i<slideList.length;i++){
+            //slideList[i].style.height=winHeight+"px";
+        }
         wrapper.style.height=winHeight+"px";
+        alert(winHeight)
+        setTimeout(function(){
+        },3000)
     }
-    var mySwiper = new Swiper('.swiper-container', {
-        speed: 400,
+    new Swiper('.swiper-container', {
         direction:"vertical",
         pagination:".swiper-pagination"
     });
